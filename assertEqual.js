ff927ff8b1cc984105d343f198234169e255cd30
hktaneja <hktaneja2020@gmail.com>
2023-06-18 14:08:06 -0500

    Added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log("👌 " + actual + " === " + expected);
+  else
+    console.log("😢 " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
+assertEqual("Winnipeg", "Winnipeg");
