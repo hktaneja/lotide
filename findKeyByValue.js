39981acbb036a0542e130ef0ce3f4453b61d1b8f
hktaneja <hktaneja2020@gmail.com>
2023-06-24 20:33:02 -0500

    added findKeyByValue.js file

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`Assertion passed ${actual} === ${expected}`);
+  else
+    console.log(`Assertion Failed ${actual} !== ${expected}`);
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(ShowObject, value) {
+  for (const item in ShowObject) {
+    if (ShowObject[item] === value) 
+    {
+      return ShowObject[item];
+    }        
+  }
+}
+  
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "The Wire");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
